b82535dedffb103b81377c44bd2a184bac11dc37
goblins <alessiomonti97@gmail.com>
2022-09-26 18:59:09 +0200

    removed entry;

--- src/Pages/Mobile/launchpad_mobile.jsx
+++ src/Pages/Mobile/launchpad_mobile.jsx
@@ -8,7 +8,6 @@ import { motion } from "framer-motion"
 //move these up the chain
 const data = [
     {price: '1.5', rarity:'LEGENDARY', stock:100, stock_num:150, stock_amt:150, access:'whitelist', time:'SOLD OUT', active:false},
-    {price: '0.12', rarity:'RARE', stock:100, stock_num:5000, stock_amt:5000, access:'whitelist', time:'SOLD OUT', active:false},
     {price: '0.12', rarity:'RARE', stock:23, stock_num:1159, stock_amt:5000, access:'public mint', time:'7D 09H 11M', active:true},
     {price: '0.39', rarity: 'EPIC', stock:false, stock_num: 650, stock_amt:650, access:'whitelist', time: 'OCTOBER', active:false},
     {price: '0.28', rarity: 'S.RARE', stock:false, stock_num: 1200, stock_amt:1200, access:'whitelist', time: 'NOVEMBER', active:false},
